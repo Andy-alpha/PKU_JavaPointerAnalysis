26da9a18413a7ca6736d94f029ce8f50f0e673de
Andy-alpha <42857663@qq.com>
2024-11-21 15:14:53 +0800

    Update Analysis in constant propagation.

--- README.md
+++ README.md
@@ -11,4 +11,28 @@
 
 - 第一个参数指定代码所在目录
 - 第二个参数指定 `main()` 方法所在的类
-- 示例 `out/test/pointerAnalysis test.Hello`
+- ~~示例 `out/test/pointerAnalysis test.Hello`~~
+- 示例：在 Tai-e 目录下用 gradle 运行 `gradle run --args="-a pku-pta-trivial -cp src/test/pku -m test.Hello"`
+
+## 打包测试 jar 文件
+
+- 在 Tai-e 目录下运行 `gradle fatJar`
+- 可以在 `build/` 文件夹下找到生成的 jar 文件
+- 把它迁移到工作目录 `/home/student/submission/submission.jar`
+- 在工作目录下创建一个 `run.sh` ，内容为 `java -jar submission.jar -a pku-pta -cp testcase -m $1`
+    - (或者直接执行 `java -jar submission.jar -a pku-pta -cp testcase -m <待测试类名>`)
+- 在工作目录目录下放置太阿所需的文件（ `java-benchmarks` 文件夹）
+- 在工作目录目录下放置待分析的代码（ `testcase` 文件夹，内含 `benchmark/` 和 `test/` 等）
+- 运行 `./run.sh <待测试的类名>`
+- 读取工作目录下的输出结果（`result.txt`）
+- 将结果与标准答案对比，根据是否 Sound 及精确度打分
+
+## 安装 & IR-Dumper（已经完成则跳过这步）
+
+1. 下载代码，建议直接从项目包中解压
+2. 在本地环境中安装OpenJDK17、Gradle
+3. 在Tai-e目录下执行:
+git clone https://github.com/pascal-lab/java-benchmarks
+4. 在Tai-e目录下执行: `gradle run --args="-a ir-dumper -cp src/test/pku -m test.Hello"`
+    - 或者执行 `java -jar tai-e-all-0.2.2.jar -a ir-dumper -cp src/test/pku -m test.Hello`
+    - 如果显示successful，说明运行成功，可在output/tir下找到输出
--- src/main/java/pku/PointerAnalysis.java
+++ src/main/java/pku/PointerAnalysis.java
@@ -1,21 +1,42 @@
 package pku;
 
+
+import java.io.*;                           // New import
+import java.util.TreeSet;                   // New import
+
+import org.apache.logging.log4j.LogManager; // New import
+import org.apache.logging.log4j.Logger;     // New import
+
 import pascal.taie.World;
 import pascal.taie.analysis.ProgramAnalysis;
+import pascal.taie.analysis.misc.IRDumper; // New import
 import pascal.taie.config.AnalysisConfig;
-public class PointerAnalysis extends PointerAnalysisTrivial
-{
+
+
+public class PointerAnalysis extends ProgramAnalysis<PointerAnalysisResult> {
     public static final String ID = "pku-pta";
+
+    private static final Logger logger = LogManager.getLogger(IRDumper.class);
+
+    /**
+     * Directory to dump Result.
+     */
+    private final File dumpPath = new File("result.txt");
 
     public PointerAnalysis(AnalysisConfig config) {
         super(config);
+        if (dumpPath.exists()) {
+            dumpPath.delete();
+        }
     }
 
     @Override
     public PointerAnalysisResult analyze() {
+        // the return value `result` is something like `line_number : alloc_number1; alloc_number2 …`
         var result = new PointerAnalysisResult();
         var preprocess = new PreprocessResult();
-        var world = World.get();
+        pascal.taie.World world = World.get();
+        // 用这里的world类型如果标注为var而不加上具体值的话，编译会报不安全
         var main = world.getMainMethod();
         var jclass = main.getDeclaringClass();
 
@@ -27,8 +48,43 @@ public class PointerAnalysis extends PointerAnalysisTrivial
         // As for when and how you enter one method,
         // it's your analysis assignment to accomplish
 
-        return super.analyze();
-        // return result;
+        world.getClassHierarchy().applicationClasses().forEach(_jclass->{
+            logger.info("Analyzing class {}", _jclass.getName());
+            _jclass.getDeclaredMethods().forEach(method->{
+                if(!method.isAbstract()) {
+                    preprocess.analysis(method.getIR());
+                    // 常量传播
+                    var const_prop_result = method.getIR().getResult("const-prop");
+                    if (const_prop_result != null) {
+                        logger.info("Got result from const-prop correctly");
+                        var cpr = (pascal.taie.analysis.dataflow.fact.DataflowResult
+                            <pascal.taie.ir.stmt.Stmt,
+                            pascal.taie.analysis.dataflow.analysis.constprop.CPFact>)const_prop_result;
+                    }
+                    else
+                        logger.info("Failed to get result from const-prop");
+                }
+            });
+        });
+
+        var objs = new TreeSet<>(preprocess.obj_ids.values());
+
+        preprocess.test_pts.forEach((test_id, pt)->{
+            result.put(test_id, objs);
+        });
+
+        dump(result);
+
+        return result;
+        //return super.analyze();
+    }
+
+    protected void dump(PointerAnalysisResult result) {
+        try (PrintStream out = new PrintStream(new FileOutputStream(dumpPath))) {
+            out.println(result);
+        } catch (FileNotFoundException e) {
+            logger.warn("Failed to dump", e);
+        }
     }
 
 }
